e235f9a786405cf42f3a9dab2044adfa556145f3
Jacob Hackett <jacobrhackett@gmail.com>
2018-01-13 17:41:09 -0500

    changing skills to map between resource type and int

--- server/src/agent/types/base_agent.py
+++ server/src/agent/types/base_agent.py
@@ -1,12 +1,13 @@
 from typing import Dict
 from server.src.agent.agents import AgentType
+from server.src.map.resources import ResourceType
 
 
 class BaseAgent:
 
     x: int
     y: int
-    resource_skills: Dict[str, int]
+    resource_skills: Dict[ResourceType, int]
     speed: int
     attack: int
     defense: int
