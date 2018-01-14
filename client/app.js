4b10a5e1dd1f60670b94f940dc7eb9f2f9d6742a
Aaron Kaplowitz <akaplowitz@carbonblack.com>
2018-01-13 19:35:44 -0500

    Add comment

--- client/app.js
+++ client/app.js
@@ -1,8 +1,28 @@
-var ws = new WebSocket("ws://localhost:8888/ws");
-ws.onopen = function() {
-   ws.send("Hello, world");
+
+// Open a socket for Player Moved updates
+var playerMovedSocket = new WebSocket("ws://localhost:8888/player_moved_ws");
+
+// Callback for successful connection
+playerMovedSocket.onopen = function() {
+   playerMovedSocket.send("UI Listening For Player Moved Updates");
+   console.log("Connected to socket, listening for Player Moved updates.")
 };
-ws.onmessage = function(message) {
+
+// Callback for receiving new message
+playerMovedSocket.onmessage = function(message) {
+    // TODO write to DOM
 	console.log(message);
 };
-console.log('hi');
+
+// Open a socket for Tile Exhausted updates
+var tileExhaustedSocket = new WebSocket("ws://localhost:8888/tile_exhausted_ws");
+
+tileExhaustedSocket.onopen = function() {
+    tileExhaustedSocket.send("UI Listening For Tile Exhausted Updates");
+    console.log("Connected to socket, listening for Tile Exhausted updates.")
+};
+
+tileExhaustedSocket.onmessage = function(message) {
+    // TODO write to DOM
+    console.log(message);
+};
--- server/socket_server.py
+++ server/socket_server.py
@@ -6,23 +6,30 @@ game_engine_message_instance = []
 tile_exhausted_instance = []
 player_moved_instance = []
 
+# To support new message types, add to this enum.
+# Each enum type has a corresponding handler class, below.
 class MessageTypes(Enum):
     TILE_EXHAUSTED = "tile_exhausted"
     PLAYER_MOVED = "player_moved"
 
+# For communicating with the game engine.  Currently only
+# receives data, does not write.
 class GameEngineMessageHandler(websocket.WebSocketHandler):
+    # Keep reference to instance to maintain singleton pattern.
     def open(self):
         if self not in game_engine_message_instance:
             game_engine_message_instance.append(self)
-
+    # Called when game engine sends us message. Forward message to correct
+    # socket handler.
     def on_message(self, message):
         for case in message.type:
             if case(MessageTypes.PLAYER_MOVED):
                 player_moved_instance[0].send_update(message.data)
             if case(MessageTypes.TILE_EXHAUSTED):
                 tile_exhausted_instance[0].send_update(message.data)
-        self.write_message(u"You said: " + message)
-        print(u"You said: " + message)
+            if case():
+                self.write_message(u"You said: " + message + " but you suck and I can't make any sense of it, sooo.")
+                print(u"Bad emum type contained in message from game engine: " + message)
 
     def on_close(self):
         if self in game_engine_message_instance:
