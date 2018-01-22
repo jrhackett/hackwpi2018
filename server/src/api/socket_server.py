5b81b7ec3ef9093e19091730d4b746efca377b2a
Jacob Hackett <jacobrhackett@gmail.com>
2018-01-22 18:51:27 -0500

    adding vue and editing server websocket files

--- server/src/api/socket_server.py
+++ server/src/api/socket_server.py
@@ -3,8 +3,8 @@ from server.src.api.websockets.websocket_factory import WebsocketFactory
 from server.src.api.websockets.websocket_topic import WebsocketTopic
 
 app = web.Application([
-    (r'/tile_exhausted_ws', WebsocketFactory.make_websocket(WebsocketTopic.TileExhaustedSocketHandler)),
-    (r'/player_moved_ws', WebsocketFactory.make_websocket(WebsocketTopic.PlayerMovedSocketHandler))
+    ('/tile_exhausted_ws', WebsocketFactory.make_websocket(WebsocketTopic.TILE_EXHAUSTED)),
+    ('/player_moved_ws', WebsocketFactory.make_websocket(WebsocketTopic.PLAYER_MOVED))
 ])
 
 if __name__ == '__main__':
